e23a0de2e821fa2f8a2f3bf01b421986fb6cc9dc
neverfake <alex.takahashi0119@gmail.com>
2026-03-16 19:00:56 -0700

    Publish site

--- src/components/ScrollProgress.tsx
+++
@@ -1,7 +0,0 @@
-import { motion, useScroll } from 'framer-motion'
-
-export function ScrollProgress() {
-  const { scrollYProgress } = useScroll()
-
-  return <motion.div className="scroll-progress" style={{ scaleX: scrollYProgress }} />
-}
